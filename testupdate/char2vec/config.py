a8342534196e1e83e567f03096037855dc9c7cd7
zambbo <80453217+zambbo@users.noreply.github.com>
2022-03-11 23:51:41 +0900

    Delete testupdate directory

--- testupdate/char2vec/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-CUR_TO_DATASET_PATH = '../dataset/'
-BENIGN_PATH = os.path.join(CUR_TO_DATASET_PATH, 'benigns.csv')
-PHISHING_PATH = os.path.join(CUR_TO_DATASET_PATH, 'phishings.csv')
-MODEL_SAVE_PATH = os.path.join(CUR_TO_DATASET_PATH, 'model/char2vec.cv')
-
